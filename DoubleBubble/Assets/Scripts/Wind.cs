b9c44713e2b6d143f81670c68e5f1d74244809a9
Ivan Olyanishin <ivan.ol.1704@gmail.com>
2025-01-24 18:10:20 +0500

    Implemeted wind

---
+++ DoubleBubble/Assets/Scripts/Wind.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind : MonoBehaviour
+{
+    [SerializeField] private float power;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Collision detected");
+        collision.GetComponent<Rigidbody2D>().AddForceAtPosition(transform.right * power, collision.ClosestPoint(transform.position));
+    }
+}
