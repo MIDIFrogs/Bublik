a56b2d8c6f37e9053e4c5f99bf83e1d8c7f2614f
MaximOdincov <124523356+MaximOdincov@users.noreply.github.com>
2025-01-27 17:10:33 +0500

    kazualLvl

--- DoubleBubble/Assets/Scripts/WhenPlayerDied.cs
+++ DoubleBubble/Assets/Scripts/WhenPlayerDied.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 public class WhenPlayerDied : MonoBehaviour
 {
     [SerializeField] private Ball prefab;
-    [SerializeField] private GameObject spawn; 
+    [SerializeField] private GameObject spawn;
+    [SerializeField] private bool isSpawnNew = false;
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
 
     private void Awake()
     {
-        SpawnPrefab();
+        if(!isSpawnNew) SpawnPrefab(spawn);
+
+    }
+    public void SpawnPrefab(GameObject spawn)
+    {
+        Ball player = Instantiate(prefab, spawn.transform.position, spawn.transform.rotation);
+        player.WhenPlayerDied = this;
+        virtualCamera.Follow = player.transform;
+
     }
     public void SpawnPrefab()
     {
-        Ball player = Instantiate(prefab, spawn.transform);
+        Ball player = Instantiate(prefab, spawn.transform.position, spawn.transform.rotation);
         player.WhenPlayerDied = this;
         virtualCamera.Follow = player.transform;
 
